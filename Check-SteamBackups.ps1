6b995e92da76ef41db55140ee22ceaad542945a6
Andreé Åkesson <43889664+FirebrawlX@users.noreply.github.com>
2025-10-08 14:54:12 +0200

    Update Check-SteamBackups.ps1

--- Check-SteamBackups.ps1
+++ Check-SteamBackups.ps1
@@ -240,7 +240,8 @@ foreach ($r in $Results) {
     $extraLink = ""
     $sinceDate = $r.LatestDate ? (Get-Date $r.LatestDate) : (Get-Date).AddYears(-1)
     $skidrowLinks = Get-SkidrowLinks -gameName $r.Name -sinceDate $sinceDate
-    if ($skidrowLinks.Count -gt 0) {
+    Write-Host "DEBUG: $($r.Name) skidrowLinks=" $skidrowLinks
+    if ($skidrowLinks.Count -gt 0 -and $skidrowLinks[0] -and $skidrowLinks[0].StartsWith('https://www.skidrowreloaded.com/')) {
         $extraLink = " <a href='" + $skidrowLinks[0] + "' target='_blank' title='SkidrowReloaded'><span style='font-size:1.2em;'>&#128279;</span></a>"
     }
     $HTML += "<tr class='$statusClass'><td>$($r.Name)</td><td>$($r.AppID)</td><td>$($r.InstalledBuild)</td><td>$($r.LatestBuild)</td><td>$($r.LatestDate)</td><td>$($r.Status)$extraLink</td></tr>`n"
